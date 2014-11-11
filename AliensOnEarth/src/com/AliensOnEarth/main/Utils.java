cb37c877438cb28291275d33318111766ce93130
Vipul DIvyanshu <vipuldivyanshu6.11@gmail.com>
2014-11-12 01:52:02 +0530

    new

---
+++ AliensOnEarth/src/com/AliensOnEarth/main/Utils.java
@@ -0,0 +1,16 @@
+package com.AliensOnEarth.main;
+
+public class Utils {
+
+	/**
+	 * Registration fields array
+	 */
+	public static String[] entries = {"Code Name","Blood Color","No.of Antennas","No. of Legs","Home Planet"};
+	
+	/**
+	 * Export menu array
+	 * Enter your plugin menu name title at the end of the array and add in the creatExportMenu its function call
+	 */
+	public static String[] menuExportString={"Register new Alien","Export to txt","Export pdf"};
+
+}
